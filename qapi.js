8839ba88667fd7b325a7f25a08b53fc3a2f35e83
Wurikos <50768680+Wurikos@users.noreply.github.com>
2020-12-02 13:54:07 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,43 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fc771f81732dd001e792e6e",
+				"5fc771f81732dd001e792e70",
+				"5fc7720504ae9f001efae853",
+				"5fc772093f0ee3001e1d3606",
+				"5fc7720c04ae9f001efae857"
+			]
+		},
+		"5fc771f81732dd001e792e6e": {
+			"id": "5fc771f81732dd001e792e6e",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fc771f81732dd001e792e70": {
+			"id": "5fc771f81732dd001e792e70",
+			"name": "index",
+			"pageUrl": "index"
+		},
+		"5fc7720504ae9f001efae853": {
+			"id": "5fc7720504ae9f001efae853",
+			"pageUrl": "about",
+			"name": "about"
+		},
+		"5fc772093f0ee3001e1d3606": {
+			"id": "5fc772093f0ee3001e1d3606",
+			"pageUrl": "contact",
+			"name": "contact"
+		},
+		"5fc7720c04ae9f001efae857": {
+			"id": "5fc7720c04ae9f001efae857",
+			"pageUrl": "info",
+			"name": "info"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
